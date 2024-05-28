a9c85427ad09e1f01eb7b723d9d74061f817304d
AJHBarber <alex8mpn@gmail.com>
2024-05-28 22:54:41 +0100

    add return in typed function

--- initial-setup/contracts/MyContract.sol
+++ initial-setup/contracts/MyContract.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.0;
 
 contract MyContract {
     string public myString = "Hello, World!";
+
+    function SayHello() public view returns (string memory) {
+        return myString;
+    }
 }
